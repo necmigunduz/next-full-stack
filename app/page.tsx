28df205609b3c5b7ec489c1b46dfb58b43fcc9b0
Necmi Gunduz <necmigunduz@gmail.com>
2023-12-14 12:09:18 +0300

    add: radix-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import { Button } from "@radix-ui/themes";
 
 export default function Home() {
-  return <div>APP</div>;
+  return <div><Button highContrast>Create a new issue</Button></div>;
 }
