062f591933364d4b22f1f256ee8b18a3bd716b23
Necmi Gunduz <necmigunduz@gmail.com>
2023-12-26 20:42:44 +0300

    add: issue page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 export default function Home() {
-  return <div><Button highContrast>Create a new issue</Button></div>;
+  return <div><Button highContrast><Link href={"/issues/new"}>Create a new issue</Link></Button></div>;
 }
