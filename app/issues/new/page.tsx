d50058b4772778d4df6c1d00da26e82e3def6142
Necmi Gunduz <necmigunduz@gmail.com>
2023-12-28 17:55:54 +0300

    config: theme-config for radix

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,31 @@
-'use client';
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import {
+  Button,
+  TextArea,
+  TextField,
+  Theme,
+  ThemePanel,
+} from "@radix-ui/themes";
+import React from "react";
 
 const NewIssuePage = () => {
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder='Title'/><br />
-        </TextField.Root>
-        <TextArea placeholder='Enter your description of this issue...'/>
-        <Button>Submit</Button>
-    </div>
-  )
-}
+    <Theme
+      appearance="light"
+      accentColor="cyan"
+      grayColor="gray"
+      radius="full"
+      className="max-w-xl space-y-3"
+    >
+      <h3>Create a new issue</h3>
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+        <br />
+      </TextField.Root>
+      <TextArea placeholder="Enter your description of this issue..." />
+      <Button>Submit</Button>
+    </Theme>
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
