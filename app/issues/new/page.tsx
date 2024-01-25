729cedc2c071b8a57654280e28de3e9510ccb5f1
Necmi Gunduz <necmigunduz@gmail.com>
2024-01-25 10:56:57 +0300

    add: error handling to issue create form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
+
 interface IssueForm {
   title: string;
   description: string;
@@ -15,6 +16,8 @@ const NewIssuePage = () => {
   const router = useRouter();
   const path = usePathname();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
+ 
   const submit = async (data: object) => {
     axios
       .post("/api/issues", data)
@@ -23,37 +26,45 @@ const NewIssuePage = () => {
           ? router.push(`/issues`)
           : (router.replace(path), Promise.reject("Something went wrong!"))
       )
-      .catch((error) =>
-        // error && alert("Title and/or description missing!")
-        {
-          console.log("Error: ", error);
-          error && alert("Something gone wrong!");
-        }
-      );
+      .catch((error) => {
+        error && setError("Please enter title (max 255 characters) and description (max 2500 characters).");
+      });
   };
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit((data) => submit(data))}
-    >
-      <h3>Create a new issue</h3>
-      <TextField.Root>
-        <TextField.Input placeholder="Title -required and maximum 255 characters!" {...register("title")} />
-        <br />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE
-            placeholder="Enter your description of this issue -required and maximum 2500 characters!"
-            {...field}
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-5">
+          <Callout.Text color="red">
+            <p>{error}</p>
+          </Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit((data) => submit(data))}
+      >
+        <h1 className="text-lg font-extrabold">Create a new issue</h1>
+        <TextField.Root>
+          <TextField.Input
+            placeholder="Title -required and maximum 255 characters!"
+            {...register("title")}
           />
-        )}
-      />
+          <br />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE
+              placeholder="Enter your description of this issue -required and maximum 2500 characters!"
+              {...field}
+            />
+          )}
+        />
 
-      <Button>Submit New Issue</Button>
-    </form>
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
